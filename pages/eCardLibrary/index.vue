e86fd9654ab1cce74d4fe7f7703c8c3562290244
luyang.sun <luyang.sun@mobilemd.cn>
2020-09-07 17:33:18 +0800

    commit

---
+++ pages/eCardLibrary/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<view class="">名片库</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="content">
+		<view class="hint">您还没有电子名片</view>
+		<view class="create">
+			<button type="primary">创建我的名片</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		height: 100vh;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	.hint {
+		padding: 10px;
+	}
+	.create button {
+		padding: 0 25px;
+		border-radius: 30px;
+	}
+</style>
