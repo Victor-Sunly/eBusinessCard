314f8ac6012d63c08f80c4f8eaf6686dd9ea4685
luyang.sun <luyang.sun@mobilemd.cn>
2020-09-11 18:56:42 +0800

    选择背景和排版

--- pages/index/compomnents/Composition.vue
+++ pages/index/compomnents/Composition.vue
@@ -2,24 +2,30 @@
 <template>
 	<view class="composition">
 		<!-- 预览 -->
-		<view class="preview public-style" :style="{background: selectBgImage?'url('+selectBgImage+')': '#ffffff'}">
+		<view class="preview public-style" :style="{background: selectedBgIndex!==null ?'url('+bgImages[selectedBgIndex]+')': '#ffffff'}">
 			预览
 		</view>
 		<!-- 背景 -->
 		<view class="bg-style public-style">
 			<view class="title">选择背景</view>
 			<scroll-view class="scroll-view_H" scroll-x="true" scroll-left="120">
-				<view @click="clickBg(null)" class="scrol-item style-1"><span></span></view>
-				<view @click="clickBg(1)" class="scrol-item style-2"><span></span></view>
-				<view @click="clickBg(2)" class="scrol-item style-3"><span></span></view>
-				<view @click="clickBg(3)" class="scrol-item style-4"><span></span></view>
-				<view @click="clickBg(4)" class="scrol-item style-5"><span></span></view>
+				<block v-for="(item,index) in bgImages" :key="index">
+					<view @click="selectedBg(index)" class="scrol-item" :class="'style-'+index"><span :class="{active: selectedBgIndex === index}"></span></view>
+				</block>
 			</scroll-view>
 		</view>
 		<!-- 板式 -->
 		<view class="type public-style">
 			<view class="title">选择板式</view>
-			板式
+			<scroll-view class="scroll-view_H" scroll-x="true" scroll-left="120">
+				<block v-for="(item,index) in typesets" :key="index">
+					<view @click="selectedTs(index)" class="scrol-item special-font">
+						{{item}}
+						<span :class="{active: selectedTsIndex === index}"></span>
+					</view>
+				</block>
+
+			</scroll-view>.
 		</view>
 		<!-- 标题 -->
 		<view class="share-title public-style">
@@ -31,21 +37,29 @@
 </template>
 
 <script>
+	
+	import bg0 from '../../../static/card0.jpg';
+	import bg1 from '../../../static/card1.jpg';
 	import bg2 from '../../../static/card2.jpg';
 	import bg3 from '../../../static/card3.jpg';
 	import bg4 from '../../../static/card4.jpg';
-	import bg5 from '../../../static/card5.jpg';
 	
 	export default {
 	    data() {
 	        return {
-	            bgImages: [null, bg2, bg3, bg4, bg5],
-				selectBgImage: null,
+	            bgImages: [bg0, bg1, bg2, bg3, bg4],
+				selectedBgIndex: 0, //默认为选择第一张背景图
+				typesets: ['简约', '通用', '商务'],
+				selectedTsIndex: 0, // 默认选择第一个板式
+				
 	        }
 	    },
 	    methods: {
-			clickBg(index) {
-				this.selectBgImage = index !== null?this.bgImages[index]:null
+			selectedBg(index) {
+				this.selectedBgIndex = index;
+			},
+			selectedTs(index) {
+				this.selectedTsIndex = index;
 			}
 	    }
 	}
@@ -71,11 +85,10 @@
 	.title {
 		font-size: 18px;
 		color: #0099FF;
-		margin: 0 0 10px 10px;
+		margin: 10px 0;
 	}
 	
 	.scroll-view_H {
-		height: 150px;
 		display: flex;
 		padding: 0 0 0 5px;
 		white-space: nowrap;
@@ -89,7 +102,9 @@
 		height: 70px;
 		margin: 5px;
 		border-radius: 10px;
-		
+		background-color: #FFFFFF;
+		text-align: center;
+		line-height: 70px;
 	}
 	.scrol-item span{
 		display: block;
@@ -99,26 +114,38 @@
 		border-radius: 50%;
 		margin: 5px;
 	}
+
 	
-	.style-1 {
-		background-color: #FFFFFF;
+	.style-0 {
+		background: url('../../../static/card0.jpg');
+		background-size: cover;
 	}
-	
+	.style-1 {
+		background: url('../../../static/card1.jpg');
+		background-size: cover;
+	}
 	.style-2 {
-		background: url(../../../static/card2.jpg);
+		background: url('../../../static/card2.jpg');
 		background-size: cover;
 	}
 	.style-3 {
-		background: url(../../../static/card3.jpg);
+		background: url('../../../static/card3.jpg');
 		background-size: cover;
 	}
 	.style-4 {
-		background: url(../../../static/card4.jpg);
+		background: url('../../../static/card4.jpg');
 		background-size: cover;
 	}
-	.style-5 {
-		background: url(../../../static/card5.jpg);
-		background-size: cover;
+	.active {
+		background-color: #0099FF;
+	}
+	
+	.special-font {
+		border: 1px solid #D8D3CB;
+		background-image: -webkit-linear-gradient(left,green,yellow,pink, blue, red 25%,green 35%,
+		    blue 50%,yellow 60%, red 75%, pink 85%,blue 100%); 
+		-webkit-text-fill-color: transparent;
+		-webkit-background-clip: text;
 	}
 
 	
